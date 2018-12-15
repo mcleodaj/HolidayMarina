3ebf4174c938233fbf61a832b838f36712fe5de4
mcleodaj <mcleoda@yahoo.com>
2018-12-15 15:40:25 -0500

    Add all updated files

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,34 @@
+require 'date'
+
 get '/' do
   file='public/gas_price.txt'
   File.readlines(file).each do |line|
     @gas_price = line
   end
+
+  d = DateTime.now
+  if d.month >= 10
+    @hours = {
+              'mon' => '8AM - 7PM',
+              'tues' => '8AM - 7PM',
+              'wed' => '8AM - 7PM',
+              'thurs' => '8AM - 4:45PM',
+              'fri' => '8AM - 7PM',
+              'sat' => '8AM - 8PM',
+              'sun' => '8AM - 8PM'
+            }
+  else
+    @hours = {
+              'mon' => '',
+              'tues' => '',
+              'wed' => '',
+              'thurs' => '',
+              'fri' => '',
+              'sat' => '',
+              'sun' => ''
+            }
+  end
+  puts @hours["mon"]
   erb :index
 end
 
@@ -16,9 +42,9 @@ get '/services' do
   erb :services
 end
 
-get '/pricing' do
+get '/storage' do
 
-  erb :pricing
+  erb :storage
 end
 
 get '/FAQ' do
